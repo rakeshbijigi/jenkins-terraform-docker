e243940b49884a6943d35e906f1d391ce7c48483
rakeshbijigi <rakeshbijigi@gmail.com>
2024-11-30 17:06:06 +0530

    added provider deails

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
-terraform {
- required_providers {
-    aws ={
-        source = "hashicorp/aws"
-        version = "~> 4.5"
-
-        required_version = "1.9.7"
-
-    }
-
-   
- } 
+provider "aws" {
+    region = "ap-south-1"
+  
 }
 
 resource "aws_instance" "myserver" {
